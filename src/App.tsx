fca0137e155d5781110f8e03e0155a989fa454df
Wen Bin <wenbin_ng@yahoo.com.sg>
2022-03-03 20:56:47 +0800

    Added mock data and props
    + Added mock data JSON
    + Added props for mock data JSON
    + Added skeleton and respective props for Card component
    + Added module declarations for png and jpg in tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import mockdata from './data/mockdata.json';
+
+const getData = () => JSON.parse(JSON.stringify(mockdata));
+
+interface MockItem {
+  prod_name: string;
+  img: string;
+  moq: number;
+  price: number;
+  unit: string;
+}
+
+interface MockCatalog {
+  items: MockItem[];
+  prod_group: string;
+}
+
+interface MockData {
+  catalog_count: number;
+  catalog: MockCatalog[];
+}
 
 function App() {
+  const data: MockData = getData();
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <div className="container">Hello World</div>
     </div>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-type Props = { null: null };
+type Props = {
+  imguri: string;
+  title: string;
+  moq: number;
+  price: number;
+  unit: string;
+};
 
 const Card = (props: Props) => {
-  return <div>Card</div>;
+  return (
+    <div>
+      <img src={props.imguri} alt="Logo" />
+      <h1>{props.title}</h1>
+      <p>MOQ {props.moq} Units</p>
+      <p>
+        SGD {props.price.toFixed(2)} /{props.unit}
+      </p>
+    </div>
+  );
 };
 
 export default Card;
